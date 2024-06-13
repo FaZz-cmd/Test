6749392bbcf83d683bc8f15a102740ff955a17ff
Hao-Wei <136253755+FaZz-cmd@users.noreply.github.com>
2024-06-13 23:19:07 +0200

    Update YTDownloader.py

--- YTDownloader.py
+++ YTDownloader.py
@@ -4,7 +4,6 @@ import tkinter.messagebox as tkmb
 from tkinter import filedialog
 import threading
 
-# Function to run the yt-dlp command in a separate thread
 def export_video():
     url = url_entry.get()
     resolution = resolution_var.get()
@@ -24,46 +23,41 @@ def export_video():
     except Exception as e:
         tkmb.showerror("Error", f"An error occurred: {str(e)}")
 
-# Function to start the thread
+
 def start_export():
     export_thread = threading.Thread(target=export_video)
     export_thread.start()
 
-# Function to select the output folder
+
 def select_output_folder():
     folder_path = filedialog.askdirectory()
     output_entry.delete(0, ctk.END)
     output_entry.insert(0, folder_path)
 
-# Initialize the application
+
 app = ctk.CTk()
 app.title("YTDownloader")
 app.geometry("500x415")
 
-# Label and entry for the URL
 url_label = ctk.CTkLabel(app, text="YouTube video URL:")
 url_label.pack(pady=10)
 url_entry = ctk.CTkEntry(app, width=400)
 url_entry.pack(pady=10)
 
-# Label and quality options
 resolution_label = ctk.CTkLabel(app, text="Quality:")
 resolution_label.pack(pady=10)
 resolution_var = ctk.StringVar(value="1080")
 resolution_options = ctk.CTkOptionMenu(app, variable=resolution_var, values=["1080", "1440", "2160"])
 resolution_options.pack(pady=10)
 
-# Label and entry for the output folder
 output_label = ctk.CTkLabel(app, text="Output Folder:")
 output_label.pack(pady=10)
 output_entry = ctk.CTkEntry(app, width=400)
 output_entry.pack(pady=10)
 
-# Button to select the output folder
 output_button = ctk.CTkButton(app, text="Select output folder", command=select_output_folder)
 output_button.pack(pady=10)
 
-# Button to export
 export_button = ctk.CTkButton(app, text="Export", command=start_export)
 export_button.pack(pady=20)
 
