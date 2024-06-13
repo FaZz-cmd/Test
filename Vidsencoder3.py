6568ef4d562c9eb3ccd9c109fd68aa048d19cbce
Hao-Wei <136253755+FaZz-cmd@users.noreply.github.com>
2024-06-13 16:19:28 +0200

    Update Vidsencoder3.py

--- Vidsencoder3.py
+++ Vidsencoder3.py
@@ -52,6 +52,7 @@ def encode_video():
         "mkv": ["aac", "flac", "mp3", "wav", "opus"]
     }
 
+    temp_audio_file = None
     if audio_format not in compatible_audio_formats[format]:
         temp_audio_file = os.path.join(output_folder, "temp_audio.aac")
         reencode_audio_command = f'ffmpeg -hide_banner -y -i "{input_value}" -c:a aac -b:a 614k "{temp_audio_file}"'
@@ -65,6 +66,11 @@ def encode_video():
     def run_ffmpeg_command():
         subprocess.run(ffmpeg_command, shell=True)
         tkmb.showinfo("Success", "Video encoded successfully!")
+        if temp_audio_file:
+            try:
+                os.remove(temp_audio_file)
+            except OSError as e:
+                tkmb.showerror("Error", f"Error removing temporary file: {e}")
 
     threading.Thread(target=run_ffmpeg_command).start()
 
@@ -79,24 +85,11 @@ format_var = ctk.StringVar(value="mp4")
 resolution_var = ctk.StringVar(value="Keep")
 
 encoder_command = {
-    "x264": "-c:v libx264 -crf 16 -preset slow -x264-params direct=spatial:me=umh -pix_fmt yuv420p",
-    "x265": "-c:v libx265 -crf 18 -preset medium -pix_fmt yuv420p",
-    "AV1": "-c:v libsvtav1 -qp 20 -preset 7 -svtav1-params tune=0:enable-tf=0:enable-overlays=1:enable-qm=1 -pix_fmt yuv420p10le",
-    "VP9": "-c:v libvpx-vp9 -b:v 0 -crf 18 -cpu-used 3 -aq-mode 1 -pix_fmt yuv420p",
+    "x264": "-c:v libx264 -b:v 35M -g 120 -keyint_min 120 -sc_threshold 0 -pix_fmt yuv420p -color_primaries bt709 -color_trc bt709 -colorspace bt709 -profile:v high -bf 2 -b_strategy 2",
+    "x265": "-c:v libx265 -b:v 30M -g 120 -keyint_min 120 -sc_threshold 0 -pix_fmt yuv420p10le -color_primaries bt709 -color_trc bt709 -colorspace bt709 -rc:v cbr",
     "ProRes": "-c:v prores_ks -profile:v 4 -vendor apl0 -bits_per_mb 8000 -pix_fmt yuva444p10le",
-    "ffv1": "-c:v ffv1 -coder 2 -context 1 -level 3 -slices 12 -g 1",
-    "h264_nvenc": "-c:v h264_nvenc -preset p6 -b_adapt 1 -rc-lookahead 30 -qp 18 -qp_cb_offset -2 -qp_cr_offset -2 -pix_fmt nv12",
-    "hevc_nvenc": "-c:v hevc_nvenc -preset p6 -b_adapt 1 -rc-lookahead 30 -qp 18 -qp_cb_offset -2 -qp_cr_offset -2 -pix_fmt nv12",
-    "Av1_nvenc": "-c:v av1_nvenc -preset p7 -tune:v hq -rc:v vbr -cq:v 12 -b:v 250M -pix_fmt nv12",
-    "h264_amf": "-c:v h264_amf -quality balanced -profile high -rc cqp -qp 20 -pix_fmt nv12",
-    "hevc_amf": "-c:v hevc_amf -quality balanced -profile main -rc cqp -qp 20 -pix_fmt nv12",
-    "av1_amf": "-c:v av1_amf -quality balanced -profile main -rc cqp -qp 20 -pix_fmt nv12",
-    "h264_qsv": "-c:v h264_qsv -preset slow -q 25 -pix_fmt nv12",
-    "hevc_qsv": "-c:v hevc_qsv -preset slow -q 25 -pix_fmt nv12",
-    "av1_qsv": "-c:v av1_qsv -preset slow -q 25 -pix_fmt nv12",
-    "av1-youtube": "-c:v libsvtav1 -b:v 30M -g 120 -keyint_min 120 -sc_threshold 0 -pix_fmt yuv420p -color_primaries bt709 -color_trc bt709 -colorspace bt709 -rc:v cbr",
-    "h264-youtube": "-c:v libx264 -b:v 35M -g 120 -keyint_min 120 -sc_threshold 0 -pix_fmt yuv420p -color_primaries bt709 -color_trc bt709 -colorspace bt709 -profile:v high -bf 2 -b_strategy 2",
-    "h265-youtube": "-c:v libx265 -b:v 30M -g 120 -keyint_min 120 -sc_threshold 0 -pix_fmt yuv420p10le -color_primaries bt709 -color_trc bt709 -colorspace bt709 -rc:v cbr"
+    "FFV1": "-c:v ffv1 -coder 2 -context 1 -level 3 -slices 12 -g 1",
+    "AV1": "-c:v libsvtav1 -b:v 30M -g 120 -keyint_min 120 -sc_threshold 0 -pix_fmt yuv420p -color_primaries bt709 -color_trc bt709 -colorspace bt709 -rc:v cbr",
 }
 
 resolution_mapping = {
@@ -108,7 +101,6 @@ resolution_mapping = {
     "8k": "-vf scale=7680:4320"
 }
 
-# Frame for input and output selection
 frame_select = ctk.CTkFrame(app)
 frame_select.grid(row=0, column=0, padx=10, pady=10, sticky="nswe")
 
@@ -129,7 +121,6 @@ video_name_entry.grid(row=2, column=1, padx=5, pady=5, sticky="w")
 keep_name_button = ctk.CTkButton(frame_select, text="Keep Video Name", command=keep_video_name)
 keep_name_button.grid(row=2, column=2, padx=5, pady=5, sticky="w")
 
-# Frame for encoding settings
 frame_settings = ctk.CTkFrame(app)
 frame_settings.grid(row=1, column=0, padx=10, pady=10, sticky="nswe")
 
@@ -148,7 +139,6 @@ ctk.CTkLabel(frame_settings, text="Resolution:").grid(row=2, column=0, padx=5, p
 resolution_combobox = ctk.CTkOptionMenu(frame_settings, variable=resolution_var, values=["Keep", "720p", "1080p", "2k", "4k", "8k"])
 resolution_combobox.grid(row=2, column=1, padx=5, pady=5, sticky="w")
 
-# Button to start encoding
 ctk.CTkButton(app, text="Encode", command=encode_video).grid(row=2, column=0, padx=10, pady=20, sticky="n")
 
 app.mainloop()
